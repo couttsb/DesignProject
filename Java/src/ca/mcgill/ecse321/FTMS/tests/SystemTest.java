a458190acbb871bf20d2a059416328a82b1a2410
TeamRow <electricerger@gmail.com>
2016-11-13 14:21:28 -0500

    Wrote the system test document. Following each of these should result in a positive/negative test document.

---
+++ Java/src/ca/mcgill/ecse321/FTMS/tests/SystemTest.java
@@ -0,0 +1,14 @@
+package ca.mcgill.ecse321.FTMS.tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SystemTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
